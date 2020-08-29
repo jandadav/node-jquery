bae6f109ed25a5b2fed447dc6bba5fe274b95f54
jandadav <janda.david@gmail.com>
2020-08-29 20:36:31 +0200

    working solution
    Signed-off-by: jandadav <janda.david@gmail.com>

---
+++ src/app2.js
@@ -0,0 +1,30 @@
+// https://medium.com/@asimmittal/using-jquery-nodejs-to-scrape-the-web-9bb5d439413b
+
+console.log("---> Running");
+
+const curl = require("curl");
+const jsdom = require("jsdom");
+const url = "https://en.wikipedia.org/wiki/Static_web_page";
+
+curl.get(url, null, (err,resp,body)=>{
+	if(resp.statusCode == 200){
+		parseData(body);
+	}
+	else{
+		//some error handling
+		console.log("error while fetching url");
+	}
+});
+
+
+function parseData(html){
+	const {JSDOM} = jsdom;
+    const dom = new JSDOM(html);
+    
+    //console.log(dom.serialize());
+
+   	const $ = (require('jquery'))(dom.window);
+
+   	console.log($('title').html());
+    
+}
