429be9193ac23b278ce8fbfb69a6053608e94d28
jandadav <janda.david@gmail.com>
2020-08-30 08:21:25 +0200

    replacement basic
    Signed-off-by: jandadav <janda.david@gmail.com>

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ xml = fs.readFileSync('res/data2.xml');
 dom = new JSDOM(xml);
 const $ = require("jquery")(dom.window);
 
+f.replaceTag($, 'tbody', '');
+f.replaceTag($, 'table', 'div');
 f.replaceTag($, 'tr', 'div');
+f.replaceTag($, 'td', 'div');
+f.replaceTag($, 'th', 'div');
 
 console.log(f.serialize($));
--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,13 @@
 let self = (module.exports = {
     
     replaceTag: function ($, tag, replace) {
-        $(tag).replaceWith(`<${replace}>`);
+        $(tag).each( (i, el) => {
+            let children = $(el).contents();
+            if (replace != '') {
+                $(el).wrap(`<${replace}>`);
+            }
+            $(el).replaceWith(children);
+        })
     },
 
     serialize: function ($) {
